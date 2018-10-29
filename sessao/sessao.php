afabe176e930e4b9a80ec4a351463c2d93e509b7
Pablo Geovanne <pablogeovanne@bol.com.br>
2018-10-29 07:56:57 -0300

    Add files via upload

---
+++ sessao/sessao.php
@@ -0,0 +1,12 @@
+﻿<?php
+	session_start();
+	if(!isset($_SESSION["oab"]) || !isset($_SESSION["senha"]))
+	 {
+		header("Location: http://inicialtrabalhista.com/adv/login.php");
+		exit;
+	}else{
+		echo "";
+		}
+		
+		
+?>
---
+++ sessao/sessao_adv.php
@@ -0,0 +1,8 @@
+﻿<?php
+	if(IsSet($_SESSION['adv_nome']))
+	$adv_nome=$_SESSION['adv_nome'];
+	if(IsSet($_SESSION['adv_cargo']))
+	$adv_cargo=$_SESSION['adv_cargo'];
+	exit;
+	
+?>
